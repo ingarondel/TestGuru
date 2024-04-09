b6599cdb7c7035cdf3ed3026c0668f0e09484fda
ingarondel <inga404@icloud.com>
2024-04-09 08:29:39 -0700

    feat: scopes,validations for answer, question, test

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,11 @@
 class Answer < ApplicationRecord
 	belongs_to :question
+	scope      :correct_answ, -> { where(correct: true)}
+	validate   :validate_number_of_answers
+
+	private
+
+	def validate_number_of_answers
+	  errors.add(:title, "Question can have 1 to 4 answers") unless question.answers.count.between?(1, 4)
+	end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -2,29 +2,18 @@ class Test < ApplicationRecord
 	belongs_to :category
 	belongs_to :author, class_name: "User", foreign_key: "user_id"
 
-	has_many :questions, dependent: :destroy
-	has_many :tests_users, dependent: :destroy
-	has_many :users, through: :tests_users, dependent: :destroy
+	has_many   :questions, dependent: :destroy
+	has_many   :tests_users, dependent: :destroy
+	has_many   :users, through: :tests_users, dependent: :destroy
 
-	validates :title, presence:   true,
-					  uniqueness: true
+	validates  :title, presence:   true,
+					  uniqueness: {scope: :level}
 
-	validates :level, numericality: { only_integer: true }, allow_nil: true, if: :ruby_test?
+	validates  :level, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
-	validate :validate_max_level, on: :create
+	default_scope, -> { joins(:category).order(title: :asc) }
 
-	# default_scope { order(created_at: :desc)}
-
-	scope :easy, -> (level) {where(level:level).order(created_at: :desc)}
-	scope :easy_ruby, -> {easy.where(title: 'Ruby')}
-
-	private
-
-	def validate_max_level
-		errors.add(:level) if level.to_i > 10
-	end
-
-	def ruby_test?
-		title.match(/Ruby/i)
-	end
+	scope      :easy, -> { where(level: 0..1).order(created_at: :desc) }
+	scope      :medium, -> { where(level: 2..4).order(created_at: :desc) }
+	scope      :hard, -> { where("level >= ?", 5).order(created_at: :desc) }
 end
