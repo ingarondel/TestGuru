47ee5ea884d2151218ce983afd4762223071b971
ingarondel <inga404@icloud.com>
2024-04-10 12:03:37 -0700

    feat: new views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,46 +1,11 @@
 class TestsController < ApplicationController
 
-	#skip_before_action :find_test, only: :show
-	before_action :find_test, only: %i[show]
-	after_action :send_log_message
-	around_action :log_execute_time
-
-	rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
-
 	def index
-		result = ["Class: #{params.class}", "Parametrs: #{params.inspect}"]
-
-		render plain: result.join("\n")
+		@tests = Test.all
 	end
 
 	def show
-		render inline: '<%= @test.title %>'
-	end
-
-	def new
-	
-	end
-
-	private
-
-	def find_test
 		@test = Test.find(params[:id])
-	end
-
-	def send_log_message
-		logger.info("Action [#{action_name}] was finished")
-	end
-
-	def log_execute_time
-		start = Time.now
-		yield
-		finish = Time.now - start
-
-		logger.info("Execution time: #{finish * 1000}ms")
-	end
-
-	def rescue_with_test_not_found
-		render plain: 'Test was not found'
 	end
 
 end
