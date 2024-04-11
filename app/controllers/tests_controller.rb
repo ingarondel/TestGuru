96e291dc4fead9e28f03680b77579efd140992e7
ingarondel <inga404@icloud.com>
2024-04-11 05:38:12 -0700

    fix: test controller,views, helper
    Was deleted by switched github branches

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,28 +1,49 @@
 class TestsController < ApplicationController
 
-	#skip_before_action :find_test, only: :show
-	before_action :find_test, only: %i[show]
-	after_action :send_log_message
-	around_action :log_execute_time
-
+	before_action :find_test, only: %i[show edit update destroy]
+	
 	rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
 	def index
-		result = ["Class: #{params.class}", "Parametrs: #{params.inspect}"]
-
-		render plain: result.join("\n")
+		@tests = Test.all
 	end
 
 	def show
-		render inline: '<%= @test.title %>'
 
-		@test = Test.find(params[:id])
-	    @questions = @test.questions
 	end
 
 	def new
+		@test = Test.new
+	end
+
+	def create
+		@test = @test.new(test_params)
+
+		if @test.save
+			redirect_to test_path(@test)
+		else
+			render :new
+		end
+	end
+
+	def edit
 	
 	end
+
+	def update
+
+		if @test.update(test_params)
+			redirect_to @test
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@test.destroy
+		redirect_to tests_path
+	end
+
 
 	private
 
@@ -30,17 +51,10 @@ class TestsController < ApplicationController
 		@test = Test.find(params[:id])
 	end
 
-	def send_log_message
-		logger.info("Action [#{action_name}] was finished")
+	def test_params
+		params.require(:test).permit(:title, :level, :category_id)
 	end
 
-	def log_execute_time
-		start = Time.now
-		yield
-		finish = Time.now - start
-
-		logger.info("Execution time: #{finish * 1000}ms")
-	end
 
 	def rescue_with_test_not_found
 		render plain: 'Test was not found'
