f252232e6c0e5165589d7eb0b57e76404a1d27c4
ingarondel <inga404@icloud.com>
2024-04-20 04:50:33 -0700

    refactor: add gem for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-
-include Auth
-
+	
 has_many   :test_passages
 has_many   :tests, through: :test_passages	
 
+has_secure_password 
 
 	def test_passage(test)
 		test_passages.order(id: :desc).find_by(test_id: test.id)
