6cdf2f5ad0e73e84a1d0349688a6840425b9e969
ingarondel <inga404@icloud.com>
2024-04-20 04:45:01 -0700

    feat: add autentification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_20_094842) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_20_111836) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
@@ -56,10 +56,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_094842) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "email", null: false
     t.boolean "admin", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
