f89e58bc5c4305e2d20b98b8646aeabf45a46a16
ingarondel <90716651+ingarondel@users.noreply.github.com>
2024-05-13 12:59:35 -0700

    feat: badge controllers for users

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,7 +1,7 @@
 class TestPassagesController < ApplicationController
 
 	before_action :authenticate_user!
-	before_action :set_test_passage, only: %i[show gist update result]
+	before_action :set_test_passage, only: %i[show update gist result]
 
 	def show
 
@@ -12,13 +12,20 @@ class TestPassagesController < ApplicationController
 	end
 
 	def update
-		@test_passage.accept!(params[:answer_ids])
-		if @test_passage.completed?
-			TestsMailer.completed_test(@test_passage).deliver_now
-			redirect_to result_test_passage_path(@test_passage)
-		else
-		render :show
-	end
+	  @test_passage.accept!(params[:answer_ids])
+	  if @test_passage.completed?
+	    TestsMailer.completed_test(@test_passage).deliver_now
+
+	    if @test_passage.success_passed == true
+	      badge = BadgesService.new(@test_passage)
+	      badge.call
+	      flash.now[:notice] = "Achievement received"
+	    end
+
+	    redirect_to result_test_passage_path(@test_passage)
+	  else
+	    render :show
+	  end
 	end
 	
   def gist
